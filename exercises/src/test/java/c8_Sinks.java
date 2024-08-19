e9cd6e05f6df6b267aea8401f4f2e0ca260db16f
krenMaksim <krenmaba@gmail.com>
2024-08-19 09:27:58 +0300

    done blue_jeans

--- exercises/src/test/java/c8_Sinks.java
+++ exercises/src/test/java/c8_Sinks.java
@@ -151,7 +151,7 @@ public class c8_Sinks extends SinksBase {
     @Test
     public void blue_jeans() {
         //todo: enable autoCancel parameter to prevent sink from closing
-        Sinks.Many<Integer> sink = Sinks.many().multicast().onBackpressureBuffer();
+        Sinks.Many<Integer> sink = Sinks.many().replay().all();
         Flux<Integer> flux = sink.asFlux();
 
         //don't change code below
